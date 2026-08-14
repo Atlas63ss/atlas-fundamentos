e8e9470b347027d9408ecd49dab17ab317551e9e
Andrea <315115408+Atlas63ss@users.noreply.github.com>
2026-08-14 11:58:08 +0200

    Actualización bucles.py

--- bucles.py
+++ bucles.py
@@ -5,5 +5,10 @@ for producto in lista_compra:
 numeros = [3, 7, 12, 20]
 
 for numero in numeros:
-    print(numero*2)
+    print(f"El doble de {numero} es {numero*2}")
 
+precios = [12, 5, 23, 8]
+total_acumulado = 0
+for precio in precios:
+    total_acumulado += precio
+print(f'El total gastado es {total_acumulado}')
